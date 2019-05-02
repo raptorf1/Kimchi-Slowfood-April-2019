3c9c26f41774f97d208135cf67b3beaa8f38073b
Carrosen <carla1.rosen@gmail.com>
2019-05-02 21:27:52 +0200

    adds unit tests for the product model, adds validations for presence and length, unit tests all going green

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+    validates :name, :description, :price, presence: true
+    validates :price, length: { maximum: 3 }
+    validates :name, length: { maximum: 20 }
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'DB table' do
+    it { is_expected.to have_db_column :id }
+    it { is_expected.to have_db_column :name }
+    it { is_expected.to have_db_column :description }
+    it { is_expected.to have_db_column :price }
+  end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_length_of :name }
+    it { is_expected.to validate_presence_of :description } 
+    it { is_expected.to validate_presence_of :price }
+    it { is_expected.to validate_length_of :price }
+    
+
+  end
+
+  describe 'Factory' do
+    it 'should have valid Factory' do
+      expect(FactoryBot.create(:product)).to be_valid 
+    end
+  end
 end
