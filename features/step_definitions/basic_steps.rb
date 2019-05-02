64b71fcbd2fec0caf2ed75923ca0d6ee0d8c9018
boa <boa.matule@gmail.com>
2019-05-02 10:40:26 +0200

    added text in the layout application view, added basic_step.rb code. second step of scenario is going green.

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -2,8 +2,8 @@ Given("I visit the landing page") do
     visit root_path
   end
   
-  Then("I should see {string}") do |string|
-    pending # Write code here that turns the phrase above into concrete actions
+  Then("I should see {string}") do |content|
+  expect(page).to have_content content 
   end
   
   Then("I click {string} link") do |string|
