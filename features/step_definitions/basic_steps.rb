006a387fc5000a8b1502ff530b94fdafe0cc1bf6
Carrosen <carla1.rosen@gmail.com>
2019-05-02 10:02:05 +0200

    creates feature test for 'visitor can navigate the site' scenario creates products controller sets product controller as root_path first step of feature, going green

---
+++ features/step_definitions/basic_steps.rb
@@ -0,0 +1,16 @@
+Given("I visit the landing page") do
+    visit root_path
+  end
+  
+  Then("I should see {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("I click {string} link") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When("I click {string} link") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
