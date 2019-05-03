00aa9b015c8bee591614de4379b23c4d8fe8582f
raptorf1 <raptor_f1@hotmail.com>
2019-05-03 14:22:23 +0200

    Installs cartify to rails, generates cartify scopes, generates cartify migrations, passes those migrations to schema.db.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :products, only: [:show]
+  mount Cartify::Engine, at: '/'
   devise_for :users
   root controller: :products, action: :index
   resources :products, only: [:index]
